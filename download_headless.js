e56401a0955258e0f43607983b6e39b5671f0a6d
Jonas Bjork <jonas.betula@gmail.com>
2023-12-04 22:29:46 +0100

    removed customdate works now in headless

--- download_headless.js
+++ download_headless.js
@@ -18,35 +18,36 @@ const fs = require('fs');
         await page.type('#ctl00_Content_txtUsername', process.env.username);
         await page.type('#ctl00_Content_txtPassword', process.env.password);
         await page.click('#ctl00_Content_btnLogin');
+        console.log("Loggin in")
         await page.waitForNavigation();
 
         await page.goto('https://svensktambulansflyg.airmaestro.net/Modules/Reporting/ReportWizard.aspx?RID=80&V=1');
 
-        const today = new Date();
-        const day = today.getDate();
-        const month = today.getMonth() + 1;
-        const year = today.getFullYear();
-        const formattedDate = `${day < 10 ? '0' : ''}${day}/${month < 10 ? '0' : ''}${month}/${year}`;
+        // const today = new Date();
+        // const day = today.getDate();
+        // const month = today.getMonth() + 1;
+        // const year = today.getFullYear();
+        // const formattedDate = `${day < 10 ? '0' : ''}${day}/${month < 10 ? '0' : ''}${month}/${year}`;
 
-        // Use evaluateHandle to get a JSHandle to the input element
-        const dateInputHandle = await page.$('#ctl00_Content_Dynamic_CurrentFilter60_rDP_Date_dateInput');
+        // // Use evaluateHandle to get a JSHandle to the input element
+        // const dateInputHandle = await page.$('#ctl00_Content_Dynamic_CurrentFilter60_rDP_Date_dateInput');
 
-        // Use the JSHandle to set the value of the input element
-        await dateInputHandle.evaluate((input, formattedDate) => {
-            input.value = formattedDate;
+        // // Use the JSHandle to set the value of the input element
+        // await dateInputHandle.evaluate((input, formattedDate) => {
+        //     input.value = formattedDate;
 
-            // Trigger an 'input' event to simulate user input
-            const inputEvent = new Event('input', { bubbles: true });
-            input.dispatchEvent(inputEvent);
+        //     // Trigger an 'input' event to simulate user input
+        //     const inputEvent = new Event('input', { bubbles: true });
+        //     input.dispatchEvent(inputEvent);
 
-            // Trigger a 'change' event to ensure any associated listeners are notified
-            const changeEvent = new Event('change', { bubbles: true });
-            input.dispatchEvent(changeEvent);
+        //     // Trigger a 'change' event to ensure any associated listeners are notified
+        //     const changeEvent = new Event('change', { bubbles: true });
+        //     input.dispatchEvent(changeEvent);
 
-            // Optionally, trigger a 'blur' event to simulate losing focus (if needed)
-            const blurEvent = new Event('blur', { bubbles: true });
-            input.dispatchEvent(blurEvent);
-        }, formattedDate);
+        //     // Optionally, trigger a 'blur' event to simulate losing focus (if needed)
+        //     const blurEvent = new Event('blur', { bubbles: true });
+        //     input.dispatchEvent(blurEvent);
+        // }, formattedDate);
 
         await new Promise(r => setTimeout(r, 3000));
         await page.waitForSelector('#ctl00_Content_Dynamic_lbExportCSV', { visible: true, timeout: 60000 });
