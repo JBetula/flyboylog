5abc712502b5f20108c1e7989651abb71263646d
Jonas Bjork <jonas.betula@gmail.com>
2023-12-19 23:14:16 +0100

    Add dataRoutes to server.js

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const { debug } = require('./debug.js')
 const logbookRoutes = require('./routes/logbookRoutes.js');
+const dataRoutes = require('./routes/dataRoutes.js');
 const connectDB = require('./db.js')
-
+// const entryinsert = require('./convert_csv_to_entry.js')
 const PORT = process.env.PORT || 3000;
 const express = require('express')
 const app = express()
 
 app.use(express.json())
-app.use('/api', logbookRoutes)
+app.use('/api/logbook', logbookRoutes)
+app.use('/api/data', dataRoutes)
 app.use('/', express.static('static'))
+
+
+// entryinsert.readCSV('./downloads/input.csv')
 
 connectDB()
 
